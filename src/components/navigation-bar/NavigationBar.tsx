68c18792377328320997324629fbb5e5abc4ebc6
Zergy <xxdarkbluedevilxx@gmail.com>
2020-06-18 10:25:47 +0300

    Navigation Bar Design

--- src/components/navigation-bar/NavigationBar.tsx
+++ src/components/navigation-bar/NavigationBar.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "./NavigationBar.scss";
-import searchIcon from "./navigation-bar-images/search.svg";
 import userIcon from "./navigation-bar-images/user-icon.png";
 import notificationBellIcon from "./navigation-bar-images/notification-bell-icon.png";
 
@@ -14,16 +13,17 @@ class NavigationBar extends React.Component <{}, NavigationBarState>{
         return (
             <nav className="navigation-bar-container">
                 <div id="nav-left">
-                    <a><span>Otaku-Fanart</span></a>
+                    <span>Otaku-Fanart</span>
                     <a>Home</a>
                     <a>About</a>
-                    <img id="search-icon" src={searchIcon} alt="search icon"/>
                     <input id="search-bar" type="search" placeholder="Search.."/>
+                    <button type="submit" id="search-button"/>
                 </div>
+
                 <div id="nav-right">
                     <a><img id="user-icon" src={userIcon} alt="user icon"/></a>
                     <a><img id="notification-bell-icon" src={notificationBellIcon} alt="notification bell"/></a>
-                    <button>SIGN IN</button>
+                    <button id="sign-in-button">SIGN IN</button>
                 </div>
             </nav>
         );
