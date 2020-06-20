25f6f532866a3ee51b0ec836e67a739c82e93037
Zergy <xxdarkbluedevilxx@gmail.com>
2020-06-20 21:17:57 +0300

    Footer Design

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.scss"
+import copyrightIcon from "./img/copyright-icon.png";
+import githubIcon from  "./img/github-icon.svg";
+
+function Footer() {
+    return(
+        <footer>
+                <img src={copyrightIcon} alt="copyright"/>
+                <span>Bogdana Yaneva</span>
+                <img src={githubIcon} alt="github icon"/>
+                <a href="https://github.com/zzergy">Zergy</a>
+        </footer>
+    );
+}
+
+export default  Footer
