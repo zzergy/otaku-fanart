4567b40312cf9f97710e11342b00656d6ec82892
Zergy <xxdarkbluedevilxx@gmail.com>
2020-06-20 21:17:33 +0300

    Additional Filter manu design
    Filter menu design

---
+++ src/components/filter-nav-bar/FilterNavigationBar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./FilterNavigationBar.scss"
+
+function FilterNavigationBar () {
+        return(
+            <nav>
+                <a>Recent</a>
+                <a>Popular</a>
+                <a>Filter by</a>
+            </nav>
+        );
+}
+
+export default FilterNavigationBar;
+
