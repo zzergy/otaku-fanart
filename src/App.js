4fe815331814c70421d6d4ea16ba18c1f4220250
Zergy <xxdarkbluedevilxx@gmail.com>
2020-06-22 22:07:40 +0300

    Implemented Components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
 import NavigationBar from "./components/navigation-bar/NavigationBar";
+import Main from "./components/main/Main";
+import FilterNavigationBar from "./components/filter-nav-bar/FilterNavigationBar";
+import Footer from "./components/footer/Footer";
 
 function App() {
-  return (
-      <div>
-          <NavigationBar/>
-      </div>
-
-  );
+    return (
+        <body>
+            <NavigationBar/>
+            <FilterNavigationBar/>
+            <Main/>
+            <Footer/>
+        </body>
+    );
 }
 
 export default App;
