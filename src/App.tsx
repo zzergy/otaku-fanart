50041c482b3fdb9390b638ab2b557c768198a2ef
Zergy <xxdarkbluedevilxx@gmail.com>
2020-06-25 15:03:40 +0300

    BrowseImage page Design added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./components/HomePage/HomePage";
+import BrowseImagesPage from "./components/BrowseImages-Page/BrowseImagesPage";
+import NavigationBar from "./components/NavigationBar/NavigationBar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
     return (
         <body>
-        <HomePage/>
-        
+        <NavigationBar/>
+        {/*<HomePage/>*/}
+        <BrowseImagesPage/>
+        <Footer/>
         </body>
     );
 }
