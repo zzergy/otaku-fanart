8686939e00e620d098055d19da304d4f3aef0f55
Zergy <xxdarkbluedevilxx@gmail.com>
2020-07-03 15:12:28 +0300

    Added routing

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -2,28 +2,31 @@ import React from "react";
 import "./NavigationBar.scss";
 import userIcon from "./navigation-bar-images/user-icon.png";
 import notificationBellIcon from "./navigation-bar-images/notification-bell-icon.png";
+import {BrowserRouter, Link, Switch, Route} from "react-router-dom";
+import HomePage from "../HomePage/HomePage";
+import Footer from "../Footer/Footer";
 
 export interface NavigationBarState {
     userIconUrl: string
     userName: string
 }
 
-class NavigationBar extends React.Component <{}, NavigationBarState>{
+class NavigationBar extends React.Component <{}, NavigationBarState> {
     render() {
         return (
             <div className="nav">
                 <div className="nav-left">
-                    <span>Otaku-Fanart</span>
-                    <a>Home</a>
-                    <a>About</a>
-                    <input className="search-bar" type="search" placeholder="Search.."/>
-                    <button type="submit" className="search-button"/>
+                        <Link to="/"><span>Otaku-FanArt</span></Link>
+                        <Link to="/">Home</Link>
+                        <Link to="/about">About</Link>
+                        <input className="search-bar" type="search" placeholder="Search.."/>
+                        <button type="submit" className="search-button"/>
                 </div>
 
                 <div className="nav-right">
-                    <a><img className="user-icon" src={userIcon} alt="user icon"/></a>
+                    <Link to="/profile"><img className="user-icon" src={userIcon} alt="user icon"/></Link>
                     <a><img className="notification-bell-icon" src={notificationBellIcon} alt="notification bell"/></a>
-                    <button className="sign-in-button">SIGN IN</button>
+                    <Link to="/register" className="sign-in-button">SIGN UP</Link>
                 </div>
             </div>
         );
