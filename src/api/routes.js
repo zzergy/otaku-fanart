a978f4291335d875258bd6f6eafe3e04964efac1
Zergy <xxdarkbluedevilxx@gmail.com>
2020-07-07 18:46:51 +0300

    Post route added

--- src/api/routes.js
+++ src/api/routes.js
@@ -2,5 +2,6 @@ const express = require("express");
 const router = express.Router();
 
 router.use('/users/', require('./users/users.routes'));
+router.use('/post/', require('./posts/posts.routes'));
 
 module.exports = router;
